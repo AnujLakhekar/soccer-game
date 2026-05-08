a5146646e129ee4630e4a1460a5836996b2da289
AnujLakhekar <anujlakhekar13@gmail.com>
2026-05-08 22:04:33 +0530

    added oscilation to the ball

--- scence/ball/ball.gd
+++ scence/ball/ball.gd
@@ -10,6 +10,7 @@ var factory = BallStateFactory.new()
 var current_state : BallState = null
 
 @onready var player_detection_area: Area2D = %playerDetectionArea
+@onready var animation: AnimationPlayer = %AnimationPlayer
 
 func _ready() -> void:
 	switch_state(State.FREEFORM)
@@ -18,7 +19,7 @@ func switch_state(state: Ball.State) -> void:
 	if current_state != null:
 		current_state.queue_free()
 	current_state = factory.get_fresh_state(state)
-	current_state.setup(self, player_detection_area, carrier)
+	current_state.setup(self, player_detection_area, carrier, animation)
 	current_state.state_transition_requested.connect(switch_state.bind())
 	current_state.name = "BallStateMacine"
 	call_deferred("add_child", current_state)
--- scence/ball/ball_states/BallState.gd
+++ scence/ball/ball_states/BallState.gd
@@ -1,14 +1,15 @@
 class_name BallState
 extends Node
 
-signal  state_transition_requested(state: BallState)
+signal  state_transition_requested(state: Ball.State)
 
 var ball : Ball = null
 var carrier :Player = null
 var player_detection_area :Area2D = null
+var animation : AnimationPlayer
 
-func setup(context: Ball, player_detection_area_cntexst: Area2D, context_carrier) -> void:
+func setup(context: Ball, player_detection_area_cntexst: Area2D, context_carrier, animationContext: AnimationPlayer) -> void:
 	ball = context
 	carrier = context_carrier
 	player_detection_area  = player_detection_area_cntexst
-	
+	animation = animationContext
