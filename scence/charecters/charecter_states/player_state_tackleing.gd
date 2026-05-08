e7f4f6c908f86e72b30aa5ef708ee8e652ec5263
AnujLakhekar <anujlakhekar13@gmail.com>
2026-05-08 13:10:59 +0530

    add tackle recovery and slide velcovity

--- scence/charecters/charecter_states/player_state_tackleing.gd
+++ scence/charecters/charecter_states/player_state_tackleing.gd
@@ -1,15 +1,22 @@
 class_name PlayerStateTackle
 extends PlayerState
 
-const TACKLE_DURATION = 200
-var tackle_start_time = Time.get_ticks_msec()
-
+const TACKLE_END_DURATION = 200
+var tackle_start_time =  Time.get_ticks_msec()
+var GROUND_FRICTION = 250.0
+var is_tackle_completed = false
 
 func _enter_tree() -> void:
 	animation.play("tackel")
+
 	
 
 func _process(delta: float) -> void:
-	if Time.get_ticks_msec() - tackle_start_time > TACKLE_DURATION:
-		state_transiction_request.emit(Player.State.RECOVERING)
+	if not is_tackle_completed:
+		player.velocity.move_toward(Vector2.ZERO, delta * GROUND_FRICTION)
+		if player.velocity == Vector2.ZERO:
+			tackle_start_time =  Time.get_ticks_msec()
+			
 		
+	if Time.get_ticks_msec() - tackle_start_time > TACKLE_END_DURATION:
+		state_transiction_request.emit(Player.State.RECOVERING)
